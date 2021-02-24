e2528938679844cd28a6a8f5b5a1f48eca532ec5
Evan Misshula <emisshula@jjay.cuny.edu>
2021-02-24 14:26:44 -0500

    changes nothing

--- src/Chart.js
+++ src/Chart.js
@@ -4,21 +4,20 @@ import { Dropdown } from "./Dropdown";
 import { Textbox } from "./Textbox";
 import { Barchart } from "./Barchart";
 
-export const Chart = ({data}) => {
-	const ranges = ["month", "week", "day"];
-
-	const text = null;
-	
-	const [bin, setBin] = useState("month");
-
-	return(
-		<div id="datavis">
-			<div id="chart">
-			</div>
-			<Dropdown id="selectButton" values = {ranges} parentCallback = {(d) => setBin(d)}/>
-			<Barchart data = {data} bin = {bin}/>
-			<Textbox text={text}/>
-		</div>
-	);
+export const Chart = ({ data }) => {
+  const ranges = ["month", "week", "day"];
+  const text = null;
+  const [bin, setBin] = useState("month");
+  return (
+    <div id="datavis">
+      <div id="chart"></div>
+      <Dropdown
+        id="selectButton"
+        values={ranges}
+        parentCallback={(d) => setBin(d)}
+      />
+      <Barchart data={data} bin={bin} />
+      <Textbox text={text} />
+    </div>
+  );
 };
-
